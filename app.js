3dd2e63cb28b04269e3c0e0a5f9dc2a9b4b981ac
Devansh Sati <thedevanshsati@gmail.com>
2022-07-22 11:42:27 +0530

    port fixed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const _ = require("lodash");
 const app = express();
 const date = require(__dirname+"/date.js");
 const day = date.getDate();
+const port = process.env.PORT || 5000
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -129,6 +130,6 @@ app.post("/delete", function(req, res){
 
 //-------------------------------------------------------------------------------------------------------------
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, ()=>{
+  console.log("Server started on port: "+port);
 });
